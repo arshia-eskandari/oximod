945c2c52ddb8cd959c7d38fff906b43c085a3310
arshia-eskandari <arshia.eskandari.3000@gmail.com>
2025-02-24 12:21:55 -0500

    added client

--- monoxide_macros/src/lib.rs
+++ monoxide_macros/src/lib.rs
@@ -1,17 +1,21 @@
 use proc_macro::TokenStream;
-use syn::{parse_macro_input, DeriveInput};
+use syn::{ parse_macro_input, DeriveInput };
 use quote::quote;
-use monoxide_core::Model;
 
 #[proc_macro_derive(MyTrait)]
 pub fn derive_model(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
-    
-    let name = &input.ident;
 
-    let expanded = quote! {
-        impl Model for #name {
-            async fn save(&self) -> 
+    let name = &input.ident;
+    
+    let expanded =
+    quote! {
+        impl ::monoxide_core::feature::model::Model for #name {
+            async fn save(&self) -> Result<(), ::monoxide_core::feature::error::MongoClientError> {
+                let client = ::monoxide_core::get_global_client()?;
+
+                Ok(())
+            }
         }
     };
 
