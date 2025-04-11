548e31e6346e6a93caf81fb7b10585ec2f8160a8
arshia-eskandari <arshia.eskandari.3000@gmail.com>
2025-04-10 22:54:28 -0400

    testing _id

--- monoxide/tests/macro_tests.rs
+++ monoxide/tests/macro_tests.rs
@@ -1,11 +1,12 @@
+use mongodb::bson::oid::ObjectId;
 use monoxide_core::feature::conn::client::set_global_client;
 use monoxide_macros::Model;
 use testresult::TestResult;
 use monoxide_core::feature::model::Model;
-use serde::{Deserialize, Serialize};
+use serde::{ Deserialize, Serialize };
 
 #[tokio::test]
-async fn saves_document_correctly() -> TestResult {
+async fn saves_document_without_id_correctly() -> TestResult {
     dotenv::dotenv().ok();
     let mongodb_uri = std::env::var("MONGODB_URI").expect("Failed to find MONGODB_URI");
 
@@ -15,15 +16,50 @@ async fn saves_document_correctly() -> TestResult {
     #[db("db_name")]
     #[collection("collection_name")]
     pub struct User {
+        #[serde(skip_serializing_if = "Option::is_none")]
+        _id: Option<ObjectId>,
         name: String,
         age: i32,
         active: bool,
     }
 
     let user = User {
+        _id: None,
         name: "Arshia".to_string(),
         age: 25,
         active: true,
+    };
+
+    user.save().await?;
+
+    Ok(())
+}
+
+#[tokio::test]
+async fn saves_document_with_id_correctly() -> TestResult {
+    use mongodb::bson::oid::ObjectId;
+
+    dotenv::dotenv().ok();
+    let mongodb_uri = std::env::var("MONGODB_URI").expect("Failed to find MONGODB_URI");
+
+    set_global_client(mongodb_uri).await.unwrap_or_else(|e| panic!("{}", e));
+
+    #[derive(Model, Serialize, Deserialize)]
+    #[db("db_name")]
+    #[collection("collection_name")]
+    pub struct User {
+        #[serde(skip_serializing_if = "Option::is_none")]
+        _id: Option<ObjectId>,
+        name: String,
+        age: i32,
+        active: bool,
+    }
+
+    let user = User {
+        _id: Some(ObjectId::new()), // explicitly set _id
+        name: "Ali".to_string(),
+        age: 30,
+        active: false,
     };
 
     user.save().await?;
