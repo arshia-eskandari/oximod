b6876b77c36648a970736f6ac5bf54f393c9c21f
arshia-eskandari <arshia.eskandari.3000@gmail.com>
2025-04-14 00:15:15 -0400

    re-export

--- monoxide/src/lib.rs
+++ monoxide/src/lib.rs
@@ -1,3 +1,4 @@
-use monoxide_core::error;
-use monoxide_core::feature;
-use monoxide_macros::Model;
+pub use monoxide_core::feature::model::Model as ModelTrait;
+pub use monoxide_core::feature::conn::client::{ get_global_client, set_global_client };
+pub use monoxide_core::error::conn_error::MongoDbError;
+pub use monoxide_macros::Model;
