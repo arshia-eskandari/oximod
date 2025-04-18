42600e51974032e698510e81c88722e6d8669c61
arshia-eskandari <arshia.eskandari.3000@gmail.com>
2025-04-17 23:52:43 -0400

    removed get_global_client

--- oximod/src/lib.rs
+++ oximod/src/lib.rs
@@ -1,4 +1,4 @@
 pub use oximod_core::feature::model::Model as ModelTrait;
-pub use oximod_core::feature::conn::client::{ get_global_client, set_global_client };
-pub use oximod_core::error::conn_error::MongoDbError;
+pub use oximod_core::feature::conn::client::set_global_client;
+pub use oximod_core::error::conn_error::OximodError;
 pub use oximod_macros::Model;
