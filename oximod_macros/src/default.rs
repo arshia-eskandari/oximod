445b2613b228f0d998ca1f3576c661946e9c3f72
Arshia Eskandari <arshia.eskandari.3000@gmail.com>
2025-08-25 07:30:20 -0400

    added single setter

--- oximod_macros/src/default.rs
+++ oximod_macros/src/default.rs
@@ -6,44 +6,39 @@ use syn::{Ident, Type};
 /// Appends a setter method for the `_id` field if it exists, using the provided
 /// setter name and accepting a MongoDB `ObjectId`.
 pub fn push_id_setter(
-    has_id_attr: bool,
     setters: &mut Vec<TokenStream>,
-    id_setter_name: String,
+    id_setter_name: &str,
 ) -> Result<(), TokenStream> {
-    if has_id_attr {
-        let id_method_ident = syn::Ident::new(&id_setter_name, proc_macro2::Span::call_site());
-        let id_setter = quote! {
-            /// Set the MongoDB ObjectId
-            pub fn #id_method_ident(mut self, id: ::oximod::_mongodb::bson::oid::ObjectId) -> Self {
-                self._id = Some(id);
-                self
-            }
-        };
-        setters.push(id_setter);
-    }
+    let id_method_ident = syn::Ident::new(&id_setter_name, proc_macro2::Span::call_site());
+    let id_setter = quote! {
+        /// Set the MongoDB ObjectId
+        pub fn #id_method_ident(mut self, id: ::oximod::_mongodb::bson::oid::ObjectId) -> Self {
+            self._id = Some(id);
+            self
+        }
+    };
+    setters.push(id_setter);
 
     Ok(())
 }
 
-/// Appends setter methods for all non-`_id` fields, generating `Option`-aware
+/// Appends setter method for all non-`_id` fields, generating `Option`-aware
 /// setters for optional types and direct setters for non-optional types.
-pub fn push_field_setters(all_fields: &[(Ident, Type)], setters: &mut Vec<TokenStream>) {
-    for (ident, ty) in all_fields.iter().filter(|(ident, _)| ident != "_id") {
-        let setter = if let Some(inner) = option_inner_type(ty) {
-            quote! {
-                pub fn #ident<T: Into<#inner>>(mut self, val: T) -> Self {
-                    self.#ident = Some(val.into());
-                    self
-                }
+pub fn push_field_setter(ident: &Ident, ty: &Type, setters: &mut Vec<TokenStream>) {
+    let setter = if let Some(inner) = option_inner_type(ty) {
+        quote! {
+            pub fn #ident<T: Into<#inner>>(mut self, val: T) -> Self {
+                self.#ident = Some(val.into());
+                self
             }
-        } else {
-            quote! {
-                pub fn #ident(mut self, val: #ty) -> Self {
-                    self.#ident = val;
-                    self
-                }
+        }
+    } else {
+        quote! {
+            pub fn #ident(mut self, val: #ty) -> Self {
+                self.#ident = val;
+                self
             }
-        };
-        setters.push(setter);
-    }
+        }
+    };
+    setters.push(setter);
 }
