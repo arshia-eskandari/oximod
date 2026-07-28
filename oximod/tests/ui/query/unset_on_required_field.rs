a4b038d785dd0d0845996348c843d27bd3e2bb0e
Arshia Eskandari <email@arshiaeskandari.com>
2026-07-28 11:00:52 -0400

    rejected unset updates on required fields

---
+++ oximod/tests/ui/query/unset_on_required_field.rs
@@ -0,0 +1,17 @@
+use mongodb::bson::oid::ObjectId;
+use oximod::{Model, Queryable};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize, Model)]
+#[db("test")]
+#[collection("users")]
+struct User {
+    #[serde(skip_serializing_if = "Option::is_none")]
+    _id: Option<ObjectId>,
+
+    name: String,
+}
+
+fn main() {
+    let _query = User::query().update_one(|user| user.name.unset());
+}
